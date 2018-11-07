c708e84b5e07723ce872434f12de5953137a45bb
sumanta <nandisumanta0@gmail.com>
2018-11-07 23:52:29 +0530

    tested v1

---
+++ Jenkinsfile
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+    environment {
+        CI = 'true'
+    }
+    stages {
+        stage('Test') {
+            steps {
+                script {
+                    docker.image('mongo').withRun { c ->
+                        docker.image('node:8-alpine').inside("--link ${c.id}:db") {
+                            sh 'npm install'
+                            sh 'cat /etc/hosts'
+                            sh 'MONGODB_HOST=db npm test'
+                            junit '**/jenkins-test-results.xml'
+                        }
+                    }
+                }
+            }
+        }
+        stage('Deliver') {
+            steps {
+                sh 'echo "done"'
+            }
+        }
+    }
+}
--- server.js
+++ server.js
@@ -4,31 +4,24 @@ let app = express();
 let mongoose = require('mongoose');
 let morgan = require('morgan');
 let bodyParser = require('body-parser');
-let port = 8080;
+let port = 3000;
 let book = require('./app/routes/book');
-let config = require('config'); //we load the db location from the JSON files
 //db options
-let options = { 
-				server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
-              }; 
+let options = { useNewUrlParser: true};
 
-//db connection      
-mongoose.connect(config.DBHost, options);
+//db connection
+mongoose.connect("mongodb://"+ (process.env.MONGODB_HOST|| "localhost")+"/books", options);
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
-//don't show the log when it is test
-if(config.util.getEnv('NODE_ENV') !== 'test') {
-	//use morgan to log at command line
-	app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
-}
+//use morgan to log at command line
+app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
 
-//parse application/json and look for raw text                                        
-app.use(bodyParser.json());                                     
-app.use(bodyParser.urlencoded({extended: true}));               
-app.use(bodyParser.text());                                    
-app.use(bodyParser.json({ type: 'application/json'}));  
+//parse application/json and look for raw text
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json'}));
 
 app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
 
